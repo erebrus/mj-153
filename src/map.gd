58f7d1e2c3f41fa3515c7f953ce3db9ea922d42c
erebrus <claudio.coelho@solenix.ch>
2024-03-01 19:46:01 +0000

    thrust animation and rotation

--- src/map.gd
+++ src/map.gd
@@ -11,18 +11,18 @@ func _process(delta):
 	pass
 
 
-func _input(event):
-	if event is InputEventMouseButton:
-		var m_event:InputEventMouseButton = event as InputEventMouseButton
-		if m_event.pressed:
-			var pos:Vector2 = get_global_mouse_position()
-			if event.button_index == MOUSE_BUTTON_LEFT:
-				Logger.info("Requested shot pos %s" % pos)
-				Events.shoot_requested.emit(pos)				
-			elif event.button_index == MOUSE_BUTTON_RIGHT:
-				Logger.info("Requested thrust pos %s" % pos)
-				Events.thrust_requested.emit()	
-		elif event.button_index == MOUSE_BUTTON_RIGHT:
-				Logger.info("Requested thrust stop " )
-				Events.thrust_stopped.emit()	
+#func _input(event):
+	#if event is InputEventMouseButton:
+		#var m_event:InputEventMouseButton = event as InputEventMouseButton
+		#if m_event.pressed:
+			#var pos:Vector2 = get_global_mouse_position()
+			#if event.button_index == MOUSE_BUTTON_LEFT:
+				#Logger.info("Requested shot pos %s" % pos)
+				#Events.shoot_requested.emit(pos)				
+			#elif event.button_index == MOUSE_BUTTON_RIGHT:
+				#Logger.info("Requested thrust pos %s" % pos)
+				#Events.thrust_requested.emit()	
+		#elif event.button_index == MOUSE_BUTTON_RIGHT:
+				#Logger.info("Requested thrust stop " )
+				#Events.thrust_stopped.emit()	
 			
--- src/player/player.gd
+++ src/player/player.gd
@@ -8,6 +8,10 @@ var shoot_impulse_force:float = 200.0
 
 var thrust_on:bool = false
 
+var target_angle:float
+
+@onready var animation_player = $AnimationPlayer
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Events.thrust_requested.connect(_on_thrust_requested)
@@ -35,10 +39,44 @@ func _on_thrust_requested():
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	if angle_difference(rotation, target_angle) < PI/60:
+		rotation = target_angle
+	else:
+		rotation=lerp_angle(rotation, target_angle, .8)
 	if thrust_on:
-		var pos = get_global_mouse_position()
-		var angle = pos.angle_to_point(global_position)
-		var impulse = Vector2.RIGHT.rotated(angle)*impulse_force
+		var impulse = Vector2.RIGHT.rotated(rotation)*-impulse_force
 		apply_force(impulse)
 	
+func _set_target_angle():
+	var pos = get_global_mouse_position()
+	target_angle = pos.angle_to_point(global_position)
 
+func _start_thrust():
+	Logger.info("thrust start")
+	thrust_on = true
+	_set_target_angle()
+	animation_player.play("thrust_start")
+	await animation_player.animation_finished
+	if thrust_on:
+		animation_player.play("thrust")
+
+func _stop_thrust():
+	Logger.info("thrust stop")
+	thrust_on = false
+	#if animation_player.is_playing() and nimation_player.current_animation == "thrust_start":				
+	animation_player.play("thrust_stop")
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		_set_target_angle()
+		return
+	if event is InputEventMouseButton:
+		var m_event:InputEventMouseButton = event as InputEventMouseButton
+		if event.button_index == MOUSE_BUTTON_RIGHT:
+			if not thrust_on and m_event.pressed:
+				_start_thrust()				
+			elif thrust_on and not m_event.pressed:
+				_stop_thrust()
+
+		
+	
