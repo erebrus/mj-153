4ef38c76a65c23bbc1f0d30e92787644ba94ca11
erebrus <claudio.coelho@solenix.ch>
2024-03-01 15:35:12 +0000

    addons and base setup

---
+++ src/common/events.gd
@@ -0,0 +1,4 @@
+extends RefCounted
+
+signal thrust_requested(target:Vector2)
+signal shoot_requested(target:Vector2)
---
+++ src/common/globals.gd
@@ -0,0 +1,30 @@
+extends Node
+
+var master_volume:float = 100
+var music_volume:float = 100
+var sfx_volume:float = 100
+
+const GameDataPath = "user://conf.cfg"
+var config:ConfigFile
+
+var debug_build := false
+var music
+
+
+
+func _ready():
+	_init_logger()	
+	Logger.info("Logger initialized.")
+
+
+func _init_logger():
+	Logger.set_logger_level(Logger.LOG_LEVEL_INFO)
+	Logger.set_logger_format(Logger.LOG_FORMAT_MORE)
+	var console_appender:Appender = Logger.add_appender(ConsoleAppender.new())
+	console_appender.logger_format=Logger.LOG_FORMAT_FULL
+	console_appender.logger_level = Logger.LOG_LEVEL_DEBUG
+	var file_appender:Appender = Logger.add_appender(FileAppender.new("res://debug.log"))
+	file_appender.logger_format=Logger.LOG_FORMAT_FULL
+	file_appender.logger_level = Logger.LOG_LEVEL_DEBUG
+
+
