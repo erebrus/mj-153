d45b5391b0c9ad39a9d548c097d1afc674db44d6
erebrus <claudio.coelho@solenix.ch>
2024-03-02 08:08:53 +0000

    merged fish changes

--- src/fish/fish.gd
+++ src/fish/fish.gd
@@ -1,8 +1,26 @@
-class_name Fish extends RigidBody2D
+extends CharacterBody2D
 
+const MAX_PLAYER_DISTANCE:=400.0
+
+@export
+var speed:=30.0
 
 @onready var sprite = $Sprite
 
+
+func _ready():
+	Events.player_position_updated.connect(_on_player_position_updated)
+	
+func _on_player_position_updated(pos:Vector2):
+	if global_position.distance_to(pos) > MAX_PLAYER_DISTANCE:
+		Logger.info("%s destroyed " % name)
+		queue_free()
+
+func _physics_process(delta):
+
+	velocity = Vector2.RIGHT.rotated(rotation) * speed
+
+	move_and_slide()
 
 func spear() -> void:
 	Logger.debug("fish speared")
