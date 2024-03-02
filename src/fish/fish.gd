2a1b8f4562f43b6452120fe3fb465db636c00f14
erebrus <claudio.coelho@solenix.ch>
2024-03-02 11:50:24 +0000

    fish run away

--- src/fish/fish.gd
+++ src/fish/fish.gd
@@ -4,14 +4,21 @@ class_name Fish
 const MAX_PLAYER_DISTANCE:=400.0
 
 @export var speed:=30.0
+@export var runs_away := true
+
 
 @onready var sprite = $Sprite
+@onready var reaction_timer = $ReactionTimer
+var last_player_position:Vector2 
 
 
 func _ready():
 	Events.player_position_updated.connect(_on_player_position_updated)
+	if not runs_away:
+		$DetectionBox/CollisionShape2D.disabled = true
 	
 func _on_player_position_updated(pos:Vector2):
+	last_player_position = pos
 	if global_position.distance_to(pos) > MAX_PLAYER_DISTANCE:
 		Logger.info("%s destroyed " % name)
 		queue_free()
@@ -31,3 +38,16 @@ func capture() -> void:
 	Logger.debug("fish captured")
 	Events.fish_captured.emit(self)
 	queue_free()
+
+
+func _on_detection_box_body_entered(body):
+	Logger.info("%s detected player")
+	reaction_timer.start()
+
+
+func _on_detection_box_body_exited(body):
+	reaction_timer.stop()
+
+func _on_reaction_timer_timeout():
+	Logger.info("%s running away")
+	rotation = last_player_position.angle_to_point(global_position)
