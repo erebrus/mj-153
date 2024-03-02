7986e28c2709abb972108fe121e694e9844611a7
Ángela <angela.lbsr@gmail.com>
2024-03-02 09:44:03 +0100

    integrate harpooning

---
+++ src/gui/gui.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+
+var fishes: int = 0
+
+@onready var fish_counter = $FishCounter
+
+
+func _ready() -> void:
+	Events.fish_captured.connect(_on_fish_captured)
+	fish_counter.text = str(fishes)
+	
+
+func _on_fish_captured(fish: Fish) -> void:
+	fishes += 1
+	fish_counter.text = str(fishes)
