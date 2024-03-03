92500b9b51840438e2cdcf771a3167595e895354
Ángela <angela.lbsr@gmail.com>
2024-03-03 12:51:59 +0100

    added box and fishes to gameover

--- src/game_over.gd
+++ src/game_over.gd
@@ -2,11 +2,14 @@ extends Node
 
 @export var asteroid_speed: float = 0.05
 
-@onready var asteroid = %Asteroid
-@onready var asteroid2 = %Asteroid2
-@onready var asteroid3 = %Asteroid3
 
-
+@onready var objects = [
+	%Asteroid,
+	%Asteroid2,
+	%Asteroid3,
+	%Asteroid4,
+	%Asteroid5
+]
 var can_exit = false
 
 
@@ -18,9 +21,8 @@ func _ready() -> void:
 	
 
 func _physics_process(delta: float) -> void:
-	asteroid.progress_ratio += delta * asteroid_speed
-	asteroid2.progress_ratio += delta * asteroid_speed
-	asteroid3.progress_ratio += delta * asteroid_speed
+	for asteroid in objects:
+		asteroid.progress_ratio += delta * asteroid_speed
 	
 
 func _input(event: InputEvent):
